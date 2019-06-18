e1ef7f7e706c411eb7161775d8e294cca437edd5
NaclFire <nacl0312@163.com>
2019-06-18 16:33:34 +0800

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,116 @@
+#ss-panel-v3-mod_UIChanges
+#Author: 十一
+#Blog: blog.67cc.cn
+#Time：2018-8-25 11:05:33
+#!/bin/bash
+
+#check root
+[ $(id -u) != "0" ] && { echo "错误: 您必须以root用户运行此脚本"; exit 1; }
+function check_system(){
+	if [[ -f /etc/redhat-release ]]; then
+		release="centos"
+	elif cat /etc/issue | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+	elif cat /proc/version | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /proc/version | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+    fi
+	bit=`uname -m`
+	if [[ ${release} == "centos" ]] && [[ ${bit} == "x86_64" ]]; then
+	echo -e "你的系统为[${release} ${bit}],检测${Green} 可以 ${Font}搭建。"
+	else 
+	echo -e "你的系统为[${release} ${bit}],检测${Red} 不可以 ${Font}搭建。"
+	echo -e "${Yellow} 正在退出脚本... ${Font}"
+	exit 0;
+	fi
+}
+function install_ss_panel_mod_UIm(){
+    yum remove httpd -y
+	yum install unzip zip git -y
+	wget -c --no-check-certificate https://raw.githubusercontent.com/NaclFire/ss-panel-v3-mod_Uim/master/lnmp1.6.zip && unzip lnmp1.6.zip && rm -rf lnmp1.6.zip && cd lnmp1.6 && chmod +x install.sh && ./install.sh lnmp
+	cd /home/wwwroot/
+	cp -r default/phpmyadmin/ .  #复制数据库
+	cd default
+	rm -rf index.html
+	yum update nss curl iptables -y
+	#克隆项目
+	git clone -b master https://github.com/NaclFire/SSPanel-Uim.git tmp && mv tmp/.git . && rm -rf tmp && git reset --hard
+	#复制配置文件
+	cp config/.config.example.php config/.config.php
+	#移除防跨站攻击(open_basedir)
+	cd /home/wwwroot/default
+	chattr -i .user.ini
+	rm -rf .user.ini
+	sed -i 's/^fastcgi_param PHP_ADMIN_VALUE/#fastcgi_param PHP_ADMIN_VALUE/g' /usr/local/nginx/conf/fastcgi.conf
+    /etc/init.d/php-fpm restart
+    /etc/init.d/nginx reload
+	#设置文件权限
+	chown -R root:root *
+	chmod -R 777 *
+	chown -R www:www storage
+	#下载配置文件
+	wget -N -P  /usr/local/nginx/conf/ --no-check-certificate "https://raw.githubusercontent.com/NaclFire/ss-panel-v3-mod_Uim/master/nginx.conf"
+	wget -N -P /usr/local/php/etc/ --no-check-certificate "https://raw.githubusercontent.com/NaclFire/ss-panel-v3-mod_Uim/master/php.ini"
+	#开启scandir()函数
+	sed -i 's/,scandir//g' /usr/local/php/etc/php.ini
+	service nginx restart #重启Nginx
+	mysql -hlocalhost -uroot -proot <<EOF
+create database sspanel;
+use sspanel;
+source /home/wwwroot/default/sql/glzjin_all.sql;
+EOF
+	cd /home/wwwroot/default
+	#安装composer
+	wget https://getcomposer.org/installer -O composer.phar
+	php composer.phar install
+	php xcat syncusers            #同步用户
+	php xcat initQQWry            #下载IP解析库
+	php xcat resetTraffic         #重置流量
+	php xcat initdownload         #下载ssr程式
+	#创建监控
+	yum -y install vixie-cron crontabs
+	rm -rf /var/spool/cron/root
+	echo 'SHELL=/bin/bash' >> /var/spool/cron/root
+	echo 'PATH=/sbin:/bin:/usr/sbin:/usr/bin' >> /var/spool/cron/root
+	echo '*/20 * * * * /usr/sbin/ntpdate pool.ntp.org > /dev/null 2>&1' >> /var/spool/cron/root
+	echo '0 0 * * * php -n /home/wwwroot/default/xcat dailyjob' >> /var/spool/cron/root
+	echo '*/1 * * * * php /home/wwwroot/default/xcat checkjob' >> /var/spool/cron/root
+	echo "*/1 * * * * php /home/wwwroot/default/xcat syncnode" >> /var/spool/cron/root
+	echo '30 22 * * * php /home/wwwroot/default/xcat sendDiaryMail' >> /var/spool/cron/root
+	/sbin/service crond restart
+}
+
+
+#常规变量
+update_time="2018年11月10日21:14:00"
+config="/root/shadowsocks/userapiconfig.py"
+
+#fonts color
+Green="\033[32m" 
+Red="\033[31m" 
+Yellow="\033[33m"
+GreenBG="\033[42;37m"
+RedBG="\033[41;37m"
+Font="\033[0m"
+
+#notification information
+Info="${Green}[Info]${Font}"
+OK="${Green}[OK]${Font}"
+Error="${Red}[Error]${Font}"
+Notification="${Yellow}[Notification]${Font}"
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+ulimit -c 0
+rm -rf script*
+clear
+check_system
+sleep 2
+install_ss_panel_mod_UIm
