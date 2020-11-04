5220826b3f2dd4df3d6c3101d02f4276d129e937
NaclFire <nacl0312@163.com>
2020-11-04 15:28:22 +0800

    Update ss-panel-node.sh
    修复失效源

--- ss-panel-node.sh
+++ ss-panel-node.sh
@@ -70,6 +70,46 @@ if [[ ${is_auto} != "y" ]]; then
     	exit 0
 	fi
 fi
+echo "Checking if there is any existing shadowsocksr server installation..."
+if [ -d "/soft/shadowsocks" ]; then
+	while :; do echo
+		echo -n "Detect exist shadowsocksr server installation! If you continue this install, all the previous configuration will be lost! Continue?(Y/N)"
+		read is_clean_old
+		if [[ ${is_clean_old} != "y" && ${is_clean_old} != "Y" && ${is_clean_old} != "N" && ${is_clean_old} != "n" ]]; then
+			echo -n "Bad answer! Please only input number Y or N"
+		elif [[ ${is_clean_old} == "y" || ${is_clean_old} == "Y" ]]; then
+			rm -rf /soft
+			break
+		else
+			exit 0
+		fi
+	done
+fi
+echo "Updatin exsit package..."
+yum clean all && rm -rf /var/cache/yum && yum update -y
+echo "Configurating EPEL release..."
+yum install epel-release -y && yum makecache
+echo "Install necessary package..."
+yum install git net-tools htop ntp -y
+echo "Disabling firewalld..."
+systemctl stop firewalld && systemctl disable firewalld
+echo "Setting system timezone..."
+timedatectl set-timezone Asia/Taipei && systemctl stop ntpd.service && ntpdate us.pool.ntp.org
+echo "Installing libsodium..."
+yum install libsodium -y
+echo "Installing Python3.6..."
+yum install python36 python36-pip -y
+echo "Installing Shadowsocksr server from GitHub..."
+mkdir /soft
+cd /tmp && git clone -b testing https://github.com/NaclFire/shadowsocks-mod.git
+mv shadowsocks-mod shadowsocks
+mv -f shadowsocks /soft
+cd /soft/shadowsocks
+pip3 install --upgrade pip setuptools
+pip3 install -r requirements.txt
+echo "Generating config file..."
+cp apiconfig.py userapiconfig.py
+cp config.json user-config.json
 if [[ ${is_auto} != "y" ]]; then
 	#Choose the connection method
 	while :; do echo
@@ -94,63 +134,6 @@ if [[ ${is_auto} != "y" ]]; then
 		fi
 	done
 fi
-if [[ ${is_auto} != "y" ]]; then
-	echo -n "Please enter WebAPI url:"
-	read webapi_url
-	echo -n "Please enter WebAPI token:"
-	read webapi_token
-	echo -n "Server node ID:"
-	read node_id
-fi
-echo "Updatin exsit package..."
-yum clean all && rm -rf /var/cache/yum && yum update -y
-echo "Install necessary package..."
-yum install epel-release -y && yum makecache
-yum install python-pip git net-tools htop ntp -y
-yum install gcc gcc-c++ gcc-g77 flex bison autoconf automake bzip2-devel zlib-devel ncurses-devel libjpeg-devel libpng-devel libtiff-devel freetype-devel pam-devel openssl-devel libxml2-devel gettext-devel pcre-devel -y
-echo "Disabling firewalld..."
-systemctl stop firewalld && systemctl disable firewalld
-echo "Setting system timezone..."
-timedatectl set-timezone Asia/Taipei && systemctl stop ntpd.service && ntpdate us.pool.ntp.org
-echo "Installing libsodium..."
-wget https://codeload.github.com/jedisct1/libsodium/tar.gz/1.0.18-RELEASE
-tar xf 1.0.18-RELEASE && cd libsodium-1.0.18-RELEASE
-./configure && make -j2 && make install
-echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
-ldconfig
-cd ../ && rm -rf libsodium* 1.0.18*
-if [ ! -d "/soft" ]; then
-	mkdir /soft
-else
-	echo "/soft directory is already exist..."
-fi
-cd /soft
-echo "Checking if there any exist Shadowsocksr server software..."
-if [ ! -d "shadowsocks" ]; then
-	echo "Installing Shadowsocksr server from GitHub..."
-	cd /tmp && git clone -b manyuser https://github.com/NimaQu/shadowsocks.git
-	mv -f shadowsocks /soft
-else
-	while :; do echo
-		echo -n "The Shadowsocksr server software is already exsit! Do you want to upgrade it?(Y/N)"
-		read is_mu
-		if [[ ${is_mu} != "y" && ${is_mu} != "Y" && ${is_mu} != "N" && ${is_mu} != "n" ]]; then
-			echo -n "Bad answer! Please only input number Y or N"
-		elif [[ ${is_mu} == "y" && ${is_mu} == "Y" ]]; then
-			echo "Upgrading Shadowsocksr server software..."
-			cd shadowsocks && git pull
-			break
-		else
-			exit 0
-		fi
-	done
-fi
-cd /soft/shadowsocks
-pip install --upgrade pip setuptools
-pip install -r requirements.txt
-echo "Generating config file..."
-cp apiconfig.py userapiconfig.py
-cp config.json user-config.json
 do_mu(){
 	if [[ ${is_auto} != "y" ]]; then
 		echo -n "Please enter MU_SUFFIX:"
@@ -162,6 +145,14 @@ do_mu(){
 	sed -i -e "s/MU_SUFFIX = 'zhaoj.in'/MU_SUFFIX = '${mu_suffix}'/g" -e "s/MU_REGEX = '%5m%id.%suffix'/MU_REGEX = '${mu_regex}'/g" userapiconfig.py
 }
 do_modwebapi(){
+	if [[ ${is_auto} != "y" ]]; then
+		echo -n "Please enter WebAPI url:"
+		read webapi_url
+		echo -n "Please enter WebAPI token:"
+		read webapi_token
+		echo -n "Server node ID:"
+		read node_id
+	fi
 	if [[ ${is_mu} == "y" || ${is_mu} == "Y" ]]; then
 		do_mu
 	fi
@@ -196,11 +187,72 @@ if [[ ${is_auto} != "y" ]]; then
 		do_glzjinmod
 	fi
 fi
-echo "Writting system config..."
-wget https://raw.githubusercontent.com/SuicidalCat/Airport-toolkit/master/ssr_node.service
-chmod 754 ssr_node.service && mv ssr_node.service /usr/lib/systemd/system
-echo "Starting SSR Node Service..."
-systemctl enable ssr_node && systemctl start ssr_node
+do_bbr(){
+	echo "Running system optimization and enable BBR..."
+	rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+	rpm -Uvh https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm
+	yum remove kernel-headers -y
+	yum --enablerepo=elrepo-kernel install kernel-ml kernel-ml-headers -y
+	grub2-set-default 0
+	echo "tcp_bbr" >> /etc/modules-load.d/modules.conf
+	cat >> /etc/security/limits.conf << EOF
+	* soft nofile 51200
+	* hard nofile 51200
+EOF
+	ulimit -n 51200
+	cat >> /etc/sysctl.conf << EOF
+	fs.file-max = 51200
+	net.core.default_qdisc = fq
+	net.core.rmem_max = 67108864
+	net.core.wmem_max = 67108864
+	net.core.netdev_max_backlog = 250000
+	net.core.somaxconn = 4096
+	net.ipv4.tcp_congestion_control = bbr
+	net.ipv4.tcp_syncookies = 1
+	net.ipv4.tcp_tw_reuse = 1
+	net.ipv4.tcp_fin_timeout = 30
+	net.ipv4.tcp_keepalive_time = 1200
+	net.ipv4.ip_local_port_range = 10000 65000
+	net.ipv4.tcp_max_syn_backlog = 8192
+	net.ipv4.tcp_max_tw_buckets = 5000
+	net.ipv4.tcp_fastopen = 3
+	net.ipv4.tcp_rmem = 4096 87380 67108864
+	net.ipv4.tcp_wmem = 4096 65536 67108864
+	net.ipv4.tcp_mtu_probing = 1
+EOF
+	sysctl -p
+}
+do_service(){
+	echo "Writting system config..."
+	wget --no-check-certificate -O ssr_node.service https://raw.githubusercontent.com/SuicidalCat/Airport-toolkit/master/ssr_node.service.el7
+	chmod 664 ssr_node.service && mv ssr_node.service /etc/systemd/system
+	echo "Starting SSR Node Service..."
+	systemctl daemon-reload && systemctl enable ssr_node && systemctl start ssr_node
+}
+while :; do echo
+	echo -n "Do you want to enable BBR feature(from mainline kernel) and optimizate the system?(Y/N)"
+	read is_bbr
+	if [[ ${is_bbr} != "y" && ${is_bbr} != "Y" && ${is_bbr} != "N" && ${is_bbr} != "n" ]]; then
+		echo -n "Bad answer! Please only input number Y or N"
+	else
+		break
+	fi
+done
+while :; do echo
+	echo -n "Do you want to register SSR Node as system service?(Y/N)"
+	read is_service
+	if [[ ${is_service} != "y" && ${is_service} != "Y" && ${is_service} != "N" && ${is_service} != "n" ]]; then
+		echo -n "Bad answer! Please only input number Y or N"
+	else
+		break
+	fi
+done
+if [[ ${is_bbr} == "y" || ${is_bbr} == "Y" ]]; then
+	do_bbr
+fi
+if [[ ${is_service} == "y" || ${is_service} == "Y" ]]; then
+	do_service
+fi
 echo "System require a reboot to complete the installation process, press Y to continue, or press any key else to exit this script."
 read is_reboot
 if [[ ${is_reboot} == "y" || ${is_reboot} == "Y" ]]; then
